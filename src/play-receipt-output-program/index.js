ce183548ddeabd68022b4bec6659045dd81823b3
giseok <river.key93@gmail.com>
2021-02-17 21:42:52 +0900

    playFor() 중간 데이터로 사용하도록 수정

--- src/play-receipt-output-program/index.js
+++ src/play-receipt-output-program/index.js
@@ -3,13 +3,23 @@ import INVOICE from "./invoices.json";
 import PLAYS from "./plays.json";
 
 const statement = (invoice, plays) => {
+  const playFor = (aPerformance) => {
+    return plays[aPerformance.playID]; // object {name, type}
+  };
+
+  const enrichPerformance = (aPerformance) => {
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(aPerformance);
+    return result;
+  };
+
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
-  return renderPlainText(statementData, plays);
+  statementData.performances = invoice.performances.map(enrichPerformance);
+  return renderPlainText(statementData);
 };
 
-const renderPlainText = (data, plays) => {
+const renderPlainText = (data) => {
   const usd = (aNumber) => {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -18,13 +28,9 @@ const renderPlainText = (data, plays) => {
     }).format(aNumber);
   };
 
-  const playFor = (aPerformance) => {
-    return plays[aPerformance.playID]; // object {name, type}
-  };
-
   const amountFor = (aPerformance) => {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy": //비극
         result = 40000;
         if (aPerformance.audience > 30)
@@ -37,7 +43,7 @@ const renderPlainText = (data, plays) => {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
 
     return result;
@@ -46,7 +52,7 @@ const renderPlainText = (data, plays) => {
   const volumeCreaditsFor = (aPerformance) => {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
 
     return result;
@@ -72,7 +78,7 @@ const renderPlainText = (data, plays) => {
   let result = `청구 내역(고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
+    result += `${perf.play.name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
   }
