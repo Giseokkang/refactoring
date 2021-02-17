d0aa8a2879052a066f1b37fc00046dc8df9a0eba
giseok <river.key93@gmail.com>
2021-02-17 20:31:47 +0900

    포인트적립함수 추출

--- src/play-receipt-output-program/index.js
+++ src/play-receipt-output-program/index.js
@@ -36,13 +36,19 @@ function statement(invoice, plays) {
     return result;
   };
 
-  for (let perf of invoice.performances) {
+  const volumeCreaditsFor = (aPerformance) => {
+    let result = 0;
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    result += Math.max(aPerformance.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
 
+    return result;
+  };
+
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreaditsFor(perf);
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
