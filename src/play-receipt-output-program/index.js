88e1aa4d029b05219b217294889d7b300e9ef666
giseok <river.key93@gmail.com>
2021-02-17 20:09:51 +0900

    변수 인라인하기

--- src/play-receipt-output-program/index.js
+++ src/play-receipt-output-program/index.js
@@ -17,8 +17,7 @@ function statement(invoice, plays) {
 
   const amountFor = (aPerformance) => {
     let result = 0;
-    const play = playFor(aPerformance);
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy": //비극
         result = 40000;
         if (aPerformance.audience > 30)
@@ -31,23 +30,23 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
 
     return result;
   };
 
   for (let perf of invoice.performances) {
-    let play = playFor(perf);
     let thisAmount = amountFor(perf);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += `${play.name}: ${format(thisAmount / 100)} (${
+    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
